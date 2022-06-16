26470457eb4b6826b1b18f8bab4b8aa4b8801fbf
Renan Bernardo <renan.brnrd@gmail.com>
2022-06-16 20:34:08 -0300

    Modelando a carreira

--- ContentContext/Career.cs
+++ ContentContext/Career.cs
@@ -2,5 +2,11 @@ namespace Fundamentos.ContentContext;
 
 public class Career : Content
 {
-    public int Courses { get; set; }
+    public Career()
+    {
+        Items = new List<CareerItem>();
+    }
+
+    public IList<CareerItem> Items { get; set; }
+    public int TotalCourses => Items.Count;
 }
