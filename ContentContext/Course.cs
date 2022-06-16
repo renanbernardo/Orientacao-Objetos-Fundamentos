4794f72f9b1174d281db058998941b1564535ca4
Renan Bernardo <renan.brnrd@gmail.com>
2022-06-16 20:22:49 -0300

    Enumeradores

--- ContentContext/Course.cs
+++ ContentContext/Course.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+using Fundamentos.ContentContext.Enums;
 
 namespace Fundamentos.ContentContext;
 
@@ -11,22 +11,6 @@ public class Course : Content
 
     public string Tag { get; set; }
     public IList<Module> Modules { get; set; }
-}
-
-public class Module
-{
-    public Module()
-    {
-        Lectures = new List<Lecture>();
-    }
-
-    public int Order { get; set; }
-    public string Title { get; set; }
-    public IList<Lecture> Lectures { get; set; }
-}
-
-public class Lecture
-{
-    public int Order { get; set; }
-    public string Title { get; set; }
+    public int DurationInMinutes { get; set; }
+    public EContentLevel Level { get; set; }
 }
---
+++ ContentContext/Lecture.cs
@@ -0,0 +1,11 @@
+using Fundamentos.ContentContext.Enums;
+
+namespace Fundamentos.ContentContext;
+
+public class Lecture
+{
+    public int Order { get; set; }
+    public string Title { get; set; }
+    public int DurationInMinutes { get; set; }
+    public EContentLevel Level { get; set; }
+}
