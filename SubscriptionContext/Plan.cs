8c82b8ef5b91d342cc8b50a00715f268efaf5ff5
Renan Bernardo <renan.brnrd@gmail.com>
2022-06-18 14:28:09 -0300

    Validando as regras de negócio

---
+++ SubscriptionContext/Plan.cs
@@ -0,0 +1,9 @@
+using Fundamentos.SharedContext;
+
+namespace Fundamentos.SubscriptionContext;
+
+public class Plan : Base
+{
+    public string Title { get; set; }
+    public decimal Price { get; set; }
+}
--- SubscriptionContext/User.cs
+++ SubscriptionContext/User.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Fundamentos.SharedContext;
 
 namespace Fundamentos.SubscriptionContext;
